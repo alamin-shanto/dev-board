aa10a9ef5dc0d4dbbc2571163a9a74d299c3d5a5
alamin-shanto <mdalaminshanto1993@gmail.com>
2025-03-01 04:11:34 +0600

    added button function

--- script/main.js
+++ script/main.js
@@ -14,4 +14,27 @@ const [week, month, day, year] = new Date().toDateString().split(" ");
 document.getElementById("week_day").textContent = week;
 document.getElementById("date").textContent = `${month} ${day} ${year}`;
 
+// addcount & subcount
+
+function updateCount(event) {
+  alert("Board updated successfully");
+  let addCount = document.getElementById("addCount");
+  let convertedAddCount = parseInt(addCount.innerText);
+
+  let subCount = document.getElementById("subCount");
+  let convertedSubCount = parseInt(subCount.innerText);
+
+  addCount.innerText = convertedAddCount + 1;
+  subCount.innerText = convertedSubCount - 1;
+
+  let button = event.target;
+  button.disabled = true;
+
+  button.classList.add(
+    "bg-gray-400",
+    "cursor-not-allowed",
+    "pointer-events-none"
+  );
+}
+
 // clear history button
